53c49e5a984a43f0dfcb7a8a2fbe98983c3b7802
Lexyth <Lexyth@Lex.speedport.ip>
2021-01-03 03:11:27 +0100

    fixed #9
    Right Mouse Button can now be used to set Flags in addition to the
    FlagMode button, which will now serve only as safety mode to avoid
    accidental left-clicks ruining a session.
    
    Fixed a bug that allowed one to place flags on already revealed fields.

--- src/FieldButton.java
+++ src/FieldButton.java
@@ -1,11 +1,14 @@
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.function.Consumer;
 
 import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 
 public class FieldButton extends JButton {
-	
+
 	private boolean flagMode = false;
 	private boolean flag = false;
 	private int value;
@@ -16,110 +19,123 @@ public class FieldButton extends JButton {
 	int idx;
 	boolean revealed = false;
 	private Runnable boomEndListener;
-	
-	FieldButton (int idx_, int i) {
-		
+
+	FieldButton(int idx_, int i) {
+
 		super(" ");
-		
+
 		this.setFocusPainted(false);
-		
+
 		setBackground(Color.BLUE);
-		
+
 		setUncoverListener(this::flagOrReveal);
-		
+
 		idx = idx_;
-		
+
 		value = i;
 	}
-	
+
 	public void setUncoverListener(Runnable listener) {
-		
+
 		for (ActionListener a : this.getActionListeners()) {
-			
+
 			removeActionListener(a);
 		}
-		
+
 		addActionListener(event -> listener.run());
+		addMouseListener(new MouseAdapter() {
+
+			public void mouseReleased(MouseEvent e) {
+				if (SwingUtilities.isRightMouseButton(e))
+					toggleFlag();
+			}
+		});
 	}
-	
+
 	public void setCheckUncoveredWinListener(Runnable listener) {
-		
+
 		checkUncoveredWinListener = listener;
 	}
-	
+
 	public void setChangeFlaggedCountListener(Consumer<Integer> listener) {
-		
+
 		changeFlaggedCountListener = listener;
 	}
-	
+
 	public void setBoomListener(Runnable listener) {
-		
+
 		boomListener = listener;
 	}
-	
+
 	public void setBoomEndListener(Runnable listener) {
-		
+
 		boomEndListener = listener;
 	}
-	
+
 	public void setFillListener(Consumer<Integer> listener) {
-		
+
 		fillListener = listener;
 	}
-	
+
 	public void reveal() {
-		
+
 		if (value == -1) {
-			
-			setText("B"); 
+
+			setText("B");
 			boomListener.run();
 			boomEndListener.run();
 			setBackground(Color.DARK_GRAY);
 			revealed = true;
-		
+
 		} else {
-			
+
 			setText(Integer.toString(value));
 			setBackground(Color.WHITE);
 			checkUncoveredWinListener.run();
 			revealed = true;
-			if (value == 0) fillListener.accept(idx);
+			if (value == 0)
+				fillListener.accept(idx);
 		}
-		
+
 		for (ActionListener a : this.getActionListeners()) {
-			
+
 			removeActionListener(a);
 		}
-		
+
 	}
-	
+
 	public void toggleFlagMode() {
-		
+
 		flagMode = !flagMode;
 
 	}
-	
+
 	public void flagOrReveal() {
-		
-		if (flagMode) toggleFlag();
-		else if (!flag) reveal();
+
+		if (flagMode)
+			toggleFlag();
+		else if (!flag)
+			reveal();
 	}
-	
+
 	private void toggleFlag() {
-		
+
+		if (revealed)
+			return;
+
 		flag = !flag;
-		
+
 		if (flag) {
 			setBackground(Color.RED);
 		} else {
 			setBackground(Color.BLUE);
 		}
-		
-		changeFlaggedCountListener.accept(idx+(flag?0:5000));
+
+		changeFlaggedCountListener.accept(idx + (flag ? 0 : 5000));
 	}
-	
+
 	public int getValue() {
-		
+
 		return value;
 	}
 
